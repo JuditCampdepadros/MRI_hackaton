2771d9750c8dc963a7a16fd55980879df1dac8db
Judit <j.campdepadros@nin.knaw.nl>
2026-04-21 17:51:00 +0200

    Keep local version of run_fmriprep.sh after upstream sync

--- scripts/run_fmriprep.sh
+++ scripts/run_fmriprep.sh
@@ -8,9 +8,9 @@
 
 
 # ── Specify desired paths ───────────────────────────────────────
-bids_root="/data03/MRI_hackaton_data/Data_collection/bids"
-deriv_root="/data03/MRI_hackaton_data/Data_collection/fmriprep"
-work_dir="./fmriprep_work_MASSIVE_DELETE_ASAP"
+bids_root="/data03/MRI_hackaton_JCB/Data_collection/bids"
+deriv_root="/data03/MRI_hackaton_JCB/Data_collection/fmriprep"
+work_dir="/data00/EmoReg_running_analyses/MRI_hackaton/fmriprep_work_MASSIVE_DELETE_ASAP"
 
 
 # ── Do not touch anything below! ────────────────────────────────
@@ -44,10 +44,11 @@ FREESURFER_HOME="/usr/local/freesurfer"
 # Total threads ≈ nprocs × omp-nthreads
 nprocs=5
 omp_nthreads=3
+# On Storm there are 2 threads per core (36), so 72 threads total. 
 
 # ── MNI target resolution 1/2/3 mm ───────────────────────────────────
-MNI_res=2
-
+MNI_res=3
+# fMRIprep defaults to 2, but since I acquire in 3 mm, so there's no point in upsampling to 2 mm.
 
 echo "=== fMRIprep started: $(date) ==="
 
@@ -61,9 +62,6 @@ fmriprep-docker \
     --fs-no-reconall \
     --fs-license-file ${FREESURFER_HOME}/license.txt \
     --output-spaces MNI152NLin2009cAsym:res-${MNI_res} \
-    --fd-spike-threshold 0.5 \
-    --dvars-spike-threshold 1.5 \
-    --ignore slicetiming \
     --nprocs ${nprocs} \
     --omp-nthreads ${omp_nthreads} \
     --write-graph \
@@ -73,3 +71,12 @@ fmriprep-docker \
     
 
 echo "=== fMRIprep finished: $(date) ==="
+
+# u: run as current user, not root
+# no-tty: disable interactive prompts 
+# fs-no-reconall: skip FreeSurfer's recon-all (surface-based processing), goes faster
+# output-spaces: specify MNI template and resolution for output
+# fd/dvars-spike-threshold: thresholds for flagging motion outliers, it creates a motion outlier predictor 
+# with all vols that exceed these thresholds, which can be used in later analyses to control for motion, default fd = 0.5, default dvars = 1.5
+# write-graph: saves a graph of the workflow, useful for debugging
+# notrack: disable anonymous usage tracking (optional, but some prefer it for privacy)
